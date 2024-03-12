ac19e0b0b7d3a1f6b00551e0d9c8b62c98846e77
Andreyka9999 <andrey@poddubnyak.com>
2024-03-12 19:18:37 +0200

    Created 6 home work

--- Home_work_6/src/main/java/org/example/Main.java
+++ Home_work_6/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        int summa = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ievadiet 5 skaitļus.");
+            int number = sc.nextInt();
+            summa = summa + number;
+        }
+        System.out.println("Skaitļu summa ir: " + summa);
+
     }
 }
