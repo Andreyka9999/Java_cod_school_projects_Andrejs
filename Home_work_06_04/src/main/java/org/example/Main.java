c9ba92267dde11457374f09a4ed0cd04b1636382
Andreyka9999 <andrey@poddubnyak.com>
2024-04-08 18:39:58 +0300

    7 home work edited

--- Home_work_06_04/src/main/java/org/example/Main.java
+++ Home_work_06_04/src/main/java/org/example/Main.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String result = sample1();
+        int result = sample1();
         System.out.println(result);
     }
-    public static String sample1() {
+    public static int sample1() {
     Scanner sc = new Scanner(System.in);
-        System.out.println("Type a text!");
+    System.out.println("Type a text!");
     String input = sc.nextLine();
-    String neccessarySimbols = "aaa";
+
     if (input.contains("aaa")) {
         System.out.println("The text contains this simbols");
+        return 1;
     } else {
         System.out.println("Text doesn`t contains neccessary simbols");
+        return 0;
     }
-    return neccessarySimbols;
     }
 }
