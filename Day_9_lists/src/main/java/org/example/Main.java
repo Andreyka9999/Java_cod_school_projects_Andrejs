0d5573be40233b2b9c88711f2dd6155215e8f561
Andreyka9999 <andrey@poddubnyak.com>
2024-04-17 20:10:27 +0300

    Day 9 task 2 created

--- Day_9_lists/src/main/java/org/example/Main.java
+++ Day_9_lists/src/main/java/org/example/Main.java
@@ -2,11 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arrays = {1, 2, 3, 4, 5};
-        sample1(arrays);
+        int[] numbers = {1, 4, 7, 2, 9, 4, 6};
+        int search = 10;
+        search(numbers, search);
+    }
 
-     }
-    public static void sample1(int[] a) {
-        System.out.println(a);
+    public static void search(int[] array, int number) {
+        boolean found = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                System.out.println("Skaitlis " + number + " atrodas indeksā " + i);
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("Skaitlis " + number + " nav atrasts.");
+        }
     }
 }
+
